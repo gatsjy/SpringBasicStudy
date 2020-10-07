c50a892fd3987e4549cc74a1a2eb2975258dbc26
Gatsjy <gkswndks123@naver.com>
2020-10-07 22:08:05 +0900

    2020-10-07, 한주안, 옵션처리

---
+++ src/test/java/hello/core/autowiredTest/AutoWiredTest.java
@@ -0,0 +1,44 @@
+package hello.core.autowiredTest;
+
+import hello.core.member.Member;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.lang.Nullable;
+
+import java.util.Optional;
+
+/**
+ * @author Gatsjy
+ * @since 2020-10-07
+ * realize dreams myself
+ */
+public class AutoWiredTest {
+
+    @Test
+    void AutoWiredOption() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(TestBean.class);
+    }
+
+    static class TestBean {
+
+        // 1. 의존관계가 없으면 메서드 자체가 호출이 되지 않는다.
+        @Autowired(required = false)
+        public void setNoBean1(Member noBean1){
+            System.out.println("noBean1 = " + noBean1);
+        }
+
+        // 2. 호출이되지만 null로 들어온다.
+        @Autowired(required = false)
+        public void setNoBean2(@Nullable Member noBean2){
+            System.out.println("noBean2 = " + noBean2);
+        }
+
+        // 3. 빈이 없으면 optional.empty로 들어온다.
+        @Autowired
+        public void setNoBean3(Optional<Member> noBean3){
+            System.out.println("noBean3 = " + noBean3);;
+        }
+    }
+}
